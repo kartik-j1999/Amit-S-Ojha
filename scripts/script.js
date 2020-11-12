4a1049743291ff0aeee29fe5fabb6f6b3b934458
Kartik Joshi <kartikj301099@gmail.com>
2020-11-13 04:50:02 +0530

    Footer, typng animation, scroll to top button and other ux tweeks

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,32 @@ $(document).ready(function () {
         } else {
             $(".navbar").removeClass("sticky");
         }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show")
+        }
     });
+
+    //slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    //Typing animation script
+    var typed = new Typed(".typing", {
+        strings : ["Astrologer", "Numerologist", "Gemologist", "Palm Reader"],
+        typeSpeed : 100,
+        backSpeed : 60,
+        loop : true
+    });
+    var typed = new Typed(".typing-2", {
+        strings : ["Astrologer", "Numerologist", "Gemologist", "Palm Reader"],
+        typeSpeed : 100,
+        backSpeed : 60,
+        loop : true
+    });
+
     // toggle menu/navbar script
     $(".menu-btn").click(function () {
         $(".navbar .menu").toggleClass("active");
@@ -35,3 +60,5 @@ $(document).ready(function () {
         },
     });
 });
+
+document.getElementById('copyr-year').appendChild(document.createTextNode(new Date().getFullYear()));
